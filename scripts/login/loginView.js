b2efff90652bb1a4b42119976785e6c124d200c9
aadneu <aadneu@gmail.com>
2023-10-17 13:00:39 +0200

    1

--- scripts/login/loginView.js
+++ scripts/login/loginView.js
@@ -1,11 +1,15 @@
 function loginView() {
 	let html = /*html*/ `
-    
-    <div>Brukernavn: <input type="text"></div>
-    <div>Passord: <input type="password"></div>
-    <div><button>Login</button> </div>
-    <div><button>Registrer ny bruker</button> </div>
-    <div> </div>
+    <div id='loginview'>
+        <div>Brukernavn: <input type="text"></div>
+        <div>Passord: <input type="password"></div>
+        <div>Husk meg: <input type="checkbox"></div>
+
+        
+
+        <div><button>Login</button> </div>
+        <div><button>Registrer ny bruker</button> </div>
+    </div>
     `;
 
 	return html;
@@ -13,13 +17,17 @@ function loginView() {
 
 function newUser(){
     let html = /*html*/ `
-    Username: <input type="text">
-    Name: <input type="text">
-    Email: <input type="text">
-    Adress: <input type="text">
-    Zipcode: <input type="text">
-    City: <input type="text">
-    Phone: <input type="number">
+    <div>Fyll ut:</div>
+    <div>Username: <input type="text"></div>
+    <div>Name: <input type="text"></div>
+
+    <div>Email: <input type="text"></div>
+    <div>Adress: <input type="text"></div>
+    <div>Zipcode: <input type="text"></div>
+    <div> City: <input type="text"></div>
+    <div> Phone: <input type="number"></div>
+    <div><button>Lag ny bruker</button></div>
+    
  
     `;
     return html;
