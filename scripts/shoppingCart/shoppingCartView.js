3c86a7741fc6b885b9de805c7a69cbf365371814
ThereseJen <141225404+ThereseJen@users.noreply.github.com>
2023-10-17 11:31:36 +0200

    xd

--- scripts/shoppingCart/shoppingCartView.js
+++ scripts/shoppingCart/shoppingCartView.js
@@ -1,15 +1,31 @@
 function shoppingCartView() {
-	let html = ``;
+let html = /*HTML*/ `
 
-	html = /*HTML*/ `
-<div class="cart-item">
-<img src= "${model.shoppingcart.items.image}">
-<h3>${model.shoppingcart.items.name}</h3>
-<p>${model.shoppingcart.items.description}</p>
-<div> Antall: ${model.shoppingcart.items.count} </div>
-<div> Pris: </div> 
-
-</div>
+<table>
+<tr>
+<th>Bilde</th>
+<th>Navn</th>
+<th>Beskrivelse</th>
+<th>Antall</th>
+<th>Pris</th>
+</tr>
 
 `;
+
+// Ignorer alt rotet hvis dere ser dette, jeg har en plan   :DDDD
+html += /*HTML*/ `  
+
+<tr>
+<div class="cart-item">
+<td><img src= "${model.shoppingcart.items[i].image}"></td>
+<td>${model.shoppingcart.items[i].name}</td>
+<p>${model.shoppingcart.items[i].description}</p>
+<div> Antall: ${model.shoppingcart.items.count} </div> 
+<div>${model.shoppingcart.items[i].count}</div>          
+<div> Pris: </div> 
+</div>
+<tr>
+`;
+
+return html;
 }
