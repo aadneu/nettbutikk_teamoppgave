46916735c822cc21164b5a7b57db31922c4a0c4d
ThereseJen <141225404+ThereseJen@users.noreply.github.com>
2023-10-20 12:51:23 +0200

    Knapper funksjon

--- scripts/shoppingCart/shoppingCartController.js
+++ scripts/shoppingCart/shoppingCartController.js
@@ -6,8 +6,15 @@ model.inputs.shoppingCart.items[index].count = currentCount + 1;
 updateSum();
 }
 
-function removeItems() {
+function removeItems(index) {
+let lessItems = document.getElementById(`count-${index}`);
+let currentCount = parseInt(lessItems.value);
 
+if (currentCount > 0) {
+lessItems.value = currentCount - 1;
+model.inputs.shoppingCart.items[index].count = currentCount - 1;
+updateSum();
+} 
 }
 
 function emptyCart() {
