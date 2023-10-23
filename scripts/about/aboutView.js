3319d0e68ef6d4356ca40ed56bb3db6e8ec44a0b
Casper-Dev <schengen2002@outlook.com>
2023-10-23 12:28:08 +0200

    fixed om oss

--- scripts/about/aboutView.js
+++ scripts/about/aboutView.js
@@ -2,9 +2,9 @@ function aboutView() {
 	let html = ``;
 	html = /*HTML*/ `
   <div class="boxes2">
-    <div class="overskrift">OM OSS</div>
-      <div class="box7">
-      <div class="boxes">
+  <div class="box7">
+  <div class="boxes">
+  <div class="overskrift">OM OSS</div>
       <div class="box1">1</div>
       <div class="box2">2</div>
       <div class="box3">3</div>
