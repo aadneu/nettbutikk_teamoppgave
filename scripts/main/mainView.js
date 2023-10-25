b94b0684724a4ef868a6700e40debbf96292bedc
Codebear <thorbjorn.hoib@hotmail.com>
2023-10-25 11:45:01 +0200

    litt mer onclicks
    Begynte å lage kommentarer for å gjøre koden leselig, endte med å gjøre den værre x))

--- scripts/main/mainView.js
+++ scripts/main/mainView.js
@@ -1,25 +1,34 @@
 function mainView() {
 	let html = ``;
-	html = /*HTML*/ `
-	<div style='display: flex;' class='container' >
-	<button onclick="carouselLeft()">←</button>
-	<image style='flex-grow: 1;' id='leftpic' src="${
+
+	let leftIndex =
 		model.data.products[
 			model.app.carousel == 0
 				? model.data.products.length - 1
 				: model.app.carousel - 1
-		].image
-	}">
-	<image style='flex-grow: 1;' src="${
-		model.data.products[model.app.carousel].image
-	}">
-	<image style='flex-grow: 1;' id='leftpic' src="${
+		];
+
+	let rightIndex =
 		model.data.products[
 			model.app.carousel == model.data.products.length - 1
 				? 0
 				: model.app.carousel + 1
-		].image
+		];
+
+	html = /*HTML*/ `
+	<div style='display: flex;' class='container' >
+	<button onclick="carouselLeft()">←</button>
+	<image onclick='viewProduct(${
+		leftIndex.id
+	})' style='flex-grow: 1;' id='leftpic' src="${leftIndex.image}">
+	<image onclick='viewProduct(${
+		model.data.products[model.app.carousel].id
+	})' style='flex-grow: 1;' src="${
+		model.data.products[model.app.carousel].image
 	}">
+	<image onclick='viewProduct(${
+		rightIndex.id
+	})' style='flex-grow: 1;' id='leftpic' src="${rightIndex.image}">
 	<button onclick="carouselRight()">→</button>
 	</div>
   <br>
